f2d7315e518877a676b2db49977e249482840d58
jetson <desgano.vital@gmail.com>
2024-11-29 22:56:18 +0800

    small changes

--- Fusion/Plot/plot.py
+++ Fusion/Plot/plot.py
@@ -6,30 +6,38 @@ import os
 def main(): 
     path = os.getcwd()
 
-    color_raw = o3d.io.read_image(
-        path + '/2011_09_26_drive_0002_sync_image_0000000005_image_02.png')
     # color_raw = o3d.io.read_image(
-    #     '/Users/kantaphat/Downloads/ECE4500J/VE450/Plot/2011_09_26_drive_0002_sync_image_0000000005_image_02.png')
-    print(color_raw)
+    #     path + '/2011_09_26_drive_0002_sync_image_0000000005_image_02.png')
+    # # color_raw = o3d.io.read_image(
+    # #     '/Users/kantaphat/Downloads/ECE4500J/VE450/Plot/2011_09_26_drive_0002_sync_image_0000000005_image_02.png')
+    # print(color_raw)
 
-    depth_raw = o3d.io.read_image(
-        # path + '/2011_09_26_drive_0002_sync_groundtruth_depth_0000000005_image_02.png')
-        '/home/jetson/Documents/VE450/VE450/Fusion/Saved/best/results/2011_09_26_drive_0002_sync_velodyne_raw_0000000005_image_02.png')
-    print(depth_raw)
+    # depth_raw = o3d.io.read_image(
+    #     # path + '/2011_09_26_drive_0002_sync_groundtruth_depth_0000000005_image_02.png')
+    #     '/home/jetson/Documents/VE450/VE450/Fusion/Saved/best/results/2011_09_26_drive_0002_sync_velodyne_raw_0000000005_image_02.png')
+    # print(depth_raw)
 
-    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
-        color_raw, depth_raw, depth_trunc=30.0 ,convert_rgb_to_intensity=False)
+    i = 0
+    color_raw = o3d.io.read_image(path + "/C"+str(i)+".png")
+    depth_raw = o3d.io.read_image(path + "/L"+ str(i)+".png")
+    pcd = []
 
-    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
-        rgbd_image,
-        o3d.camera.PinholeCameraIntrinsic(
-            o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
-        )
-    )
-    # Flip it, otherwise the pointcloud will be upside down
-    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    while(i<2): # idk if this will work 
+        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            color_raw, depth_raw, depth_trunc=30.0 ,convert_rgb_to_intensity=False)
+
+        pcd.append(o3d.geometry.PointCloud.create_from_rgbd_image(
+            rgbd_image,
+            o3d.camera.PinholeCameraIntrinsic(
+                o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
+            )
+        ).transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]]))
+        # Flip it, otherwise the pointcloud will be upside down
+        #pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+        i = i+1 
+
     o3d.visualization.draw_geometries(
-        [pcd],
+        pcd,
         width=2000,
         height=1200,
         point_show_normal=True,

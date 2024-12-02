c8971de80f65953c583a7469e4b07566ba131a9e
Lili <sofia.velasq.shum@gmail.com>
2024-12-02 21:51:26 +0800

    added some testing for point cloud animation

--- Fusion/Plot/plot.py
+++ Fusion/Plot/plot.py
@@ -20,24 +20,27 @@ def main():
     i = 0
     color_raw = o3d.io.read_image(path + "/C"+str(i)+".png")
     depth_raw = o3d.io.read_image(path + "/L"+ str(i)+".png")
-    pcd = []
 
-    while(i<2): # idk if this will work 
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    geometry = o3d.geometry.PointCloiud()
+
+    while((not color_raw.is_empty()) and (not depth_raw.is_empty())): #reads through the entire video sequence
         rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
             color_raw, depth_raw, depth_trunc=30.0 ,convert_rgb_to_intensity=False)
 
-        pcd.append(o3d.geometry.PointCloud.create_from_rgbd_image(
+        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
             rgbd_image,
             o3d.camera.PinholeCameraIntrinsic(
                 o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
             )
-        ).transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]]))
+        ).transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
         # Flip it, otherwise the pointcloud will be upside down
         #pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
         i = i+1 
 
     o3d.visualization.draw_geometries(
-        pcd,
+        [pcd],
         width=2000,
         height=1200,
         point_show_normal=True,
